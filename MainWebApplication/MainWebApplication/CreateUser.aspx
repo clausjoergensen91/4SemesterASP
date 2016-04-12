87e55526aecc01d4c72e44d644fd271f611a3c9d
Claus Jørgensen <clausjoergensen91@gmail.com>
2016-04-12 09:55:44 +0200

    added logon and createuser aspx

---
+++ MainWebApplication/MainWebApplication/CreateUser.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/mpDefault.Master" AutoEventWireup="true" CodeBehind="CreateUser.aspx.cs" Inherits="MainWebApplication.CreateUser" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
--- MainWebApplication/MainWebApplication/mpDefault.Master.designer.cs
+++ MainWebApplication/MainWebApplication/mpDefault.Master.designer.cs
@@ -58,6 +58,24 @@ namespace MainWebApplication {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl menuItem4;
         
         /// <summary>
+        /// menuItem5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl menuItem5;
+        
+        /// <summary>
+        /// menuItem6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl menuItem6;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
